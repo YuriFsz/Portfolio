8d24edf9c72d2b56c6983514942fa9360404a870
Yuri Fernandes de Souza <98249765+YuriFsz@users.noreply.github.com>
2022-08-08 20:13:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// FILTES TABS
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tc => {
+      /* tc = tabcontent */
+      tc.classList.remove('filters__active')
+    })
+    target.classList.add('filters__active')
+
+    tabs.forEach(t => {
+      /*t = tab */
+      t.classList.remove('filter-tab-active')
+    })
+    tab.classList.add('filter-tab-active')
+  })
+})
+
+// DARK LIGHT THEMES
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+//Previously selected topic (if user selected one)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by vlidating the dark-theme classList
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+//We validate if the user previously choose a topic and
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to  know if we activated or desactivared the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === 'ri-sun-line' ? 'add' : 'remove'](
+    iconTheme
+  )
+}
+
+//Activate / desactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  //Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  //We sabe the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('select-icon', getCurrentIcon())
+})
+
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, { delay: 500 })
+sr.reveal(`.profile__profession`, { delay: 600 })
+sr.reveal(`.profile__social`, { delay: 700 })
+sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 })
+sr.reveal(`.profile__buttons`, { delay: 800 })
+sr.reveal(`.filters__content`, { delay: 900 })
+sr.reveal(`.filters`, { delay: 1000 })
